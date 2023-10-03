dc4d0bf938d383b96591f129838440ced679de54
Yulia Korolyova <yukorolyova1@yandex.ru>
2023-10-03 15:23:13 +0300

    111

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,17 +11,15 @@ const cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
 
     return res.end();
   }
-
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   return next();
 };
 
