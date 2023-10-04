3755b80e8a93bb16b3f0ce7154625f1b648df4ff
Yulia Korolyova <yukorolyova1@yandex.ru>
2023-10-04 16:55:41 +0300

    1111

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const { MONGO } = process.env;
 const express = require('express');
 const cors = require('cors');
 // const path = require('path');
@@ -23,14 +24,11 @@ const { PORT = 3000 } = process.env;
 const app = express();
 // app.use(express.static(path.join(__dirname, 'frontend')));
 
-app.use(cors({
-  origin: ['https://yuliakorolyova.students.nomoredomainsrocks.ru', 'http://yuliakorolyova.students.nomoredomainsrocks.ru', 'http://localhost:3001',
-  ],
-}));
+app.use(cors());
 app.use(requestLogger);
 
 app.use(express.json());
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb').then(() => {
+mongoose.connect(MONGO).then(() => {
   console.log('БД подключена');
 }).catch(() => {
   console.log('Не удалось подключиться к БД');
