70a9bfda0f8931a077271c4f4b7486c3a80c104f
Yulia Korolyova <yukorolyova1@yandex.ru>
2023-10-04 13:17:11 +0300

    1.17

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFound = require('./errors/NotFound');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
